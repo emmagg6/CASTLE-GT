e87b3caa0fcdd50c1f3f6e267be1542460d71e90
Emma <125744177+emmagg6@users.noreply.github.com>
2024-03-13 11:50:09 -0400

    update q-learners

--- MARL/gt-marl.py
+++ MARL/gt-marl.py
@@ -6,6 +6,10 @@ from MARL.environment import Environment
 from MARL.game import APIState
 from MARL.game import APIGame
 
+from open_spiel.python import rl_environment
+from open_spiel.python import rl_tools
+from open_spiel.python.algorithms import tabular_qlearner
+
 ################ INITIALIZATOINS ###############
 player_count = 2
 host_count = 3
@@ -26,6 +30,16 @@ w_red = np.array([0.75, 1.0])
 env = Environment(player_count, host_count, critical_count, 
                   connections, infected_hosts, infected_criticals,
                   w_blue, w_red)
+
+
+############# AGENTS ########
+player_count = 2
+num_actions_per_agent = [5, 2]
+
+agents = [
+    tabular_qlearner.QLearner(player_id = idx, num_actions = num_actions_per_agent[idx])
+    for idx in range(player_count)
+]
 
 ################ GAME ################
 security_game = APIGame(env, max_rounds=5)
