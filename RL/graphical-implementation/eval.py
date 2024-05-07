50768e0d2d17b75d62958a7375fcd2470d926d0f
em <emma.graham.th@dartmouth.edu>
2024-05-07 12:37:45 -0400

    graphical training

--- RL/graphical-implementation/eval.py
+++ RL/graphical-implementation/eval.py
@@ -2,23 +2,55 @@ from runs.loading import loading
 import matplotlib.pyplot as plt
 import numpy as np
 import networkx as nx
+import pandas as pd
 
+folder = '/scratch/egraham/CASTLE-GT/RL/graphical-implementation/runs'
 load = loading()
-load.load()
+load.load_all(path = folder)
 
 dists = load.distances
 paths = load.paths
 Qs = load.Q_tables
 cces = load.cces
-precentages = load.cce_precents
+prop = load.cce_precents
 zetas = load.zetas
+zetas_lst = load.list_zetas
+graph = load.graph
 
 '''
 Plot the precentage of cce actions per zeta value
 '''
+# print(f"Zetas: {zetas}")
+# print(f"Percentage: {prop}")
+# plt.scatter(zetas_lst, prop, color='black', alpha=0.1)
+# plt.xlabel('Zeta Value')
+# plt.ylabel('Percentage of CCE Actions')
 
-plt.scatter(zetas, precentages, color='black', alpha=0.1)
-plt.xlabel('Zeta Value')
-plt.ylabel('Percentage of CCE Actions')
+# plt.savefig('cce_percentage.png')
 
-plt.savefig('cce_precentage.png')
+
+
+'''
+Plot the distances of the paths by the zeta values
+'''
+
+data = pd.DataFrame({
+    'Zeta': zetas_lst,
+    'Distance': dists
+})
+
+data_exploded = data.apply(pd.Series.explode)
+mean_dists = data_exploded.groupby('Zeta').mean()
+std_devs = data_exploded.groupby('Zeta').std()
+
+plt.figure(figsize=(10, 6))
+plt.errorbar(mean_dists.index, mean_dists['Distance'], yerr=std_devs['Distance'], fmt='o:', capsize=5, color='black', label='RLxCCE Agent')
+
+plt.title('Average Distance vs CCE Certainty')
+plt.xlabel('Minimum observation-action visits (certainty) of CCE')
+plt.ylabel('Distance to Goal State')
+plt.grid(True)
+plt.legend(loc='upper right', fontsize='small')
+
+plt.savefig('distance_vs_cce.png')
+
--- RL/graphical-implementation/test-SARSAxCCE.py
+++ RL/graphical-implementation/test-SARSAxCCE.py
@@ -24,6 +24,11 @@ from scipy.spatial import KDTree
 
 import os
 
+from runs.loading import loading
+
+folder = '/scratch/egraham/CASTLE-GT/RL/graphical-implementation/runs'
+load = loading()
+load.load_train(path = folder)
 
 
 
@@ -345,252 +350,6 @@ def create_q_table(state_actions_dist, init_value=1.0):
 
 
 
-############################### TRAINING #################################
-
-'''
-SARSA Q-table and Agent-Agnostic EXP3-IX algorithm training
-
-Note: actions are a string given by a rotation system -- see paper for explanation
-
-    *OPTIONAL CAPABILITY*
-RETRAINING / DIFFERENT GRAPH : if the Q-table has been pretrained and already 
-contains some knowledge about the environment, setting a high initial Q-value 
-for new state-action pairs could potentially cause some issues. In particular, 
-if the new state-action pairs are actually not very good (i.e., they lead to 
-states with low rewards), then initializing them with high Q-values could cause 
-the agent to prefer these new, unexplored actions over the actions it has already 
-learned to be good. This could lead to suboptimal behavior until the Q-values of 
-the new state-action pairs are updated downwards.
-'''
-
-def get_index(graph, coordinates):
-    for node, data in graph.nodes(data=True):
-        if np.array_equal(data['pos'], coordinates):
-            return node
-    raise ValueError("Coordinates not found in graph.")
-
-
-def action_selection(graph, state_actions_dist, q_table, state, previous_state, epsilon=0.1):
-    """
-    Select an action from the state using epsilon-greedy action selection.
-    """
-    # Extract the actions from (action, distance) pairs
-    state_actions = {state: [action_dist[0] for action_dist in action_dists] for state, action_dists in state_actions_dist.items()}
-
-    if np.random.rand() < epsilon:
-        # Action going back
-        back = [a for a in range(len(state_actions[state])) if state_actions[state][a] == previous_state][0]
-        # print('back', back)
-
-        # Action left & right
-        if len(state_actions[state]) == 2:
-            forward = (back + 1) % len(state_actions[state])
-            viable_actions = {'forward': forward, 'back': back}
-        elif len(state_actions[state]) == 3 :
-            left = (back + 1) % len(state_actions[state])
-            right = (back - 1) % len(state_actions[state])
-            # Viable actions is a dictionary from 'left', 'right', and 'backward' and are mapped to their back, left, and right indices in the adjacency dict
-            viable_actions = {'left': left, 'right': right, 'back': back}
-        elif len(state_actions[state]) == 4 :
-            left = (back + 1) % len(state_actions[state])
-            right = (back - 1) % len(state_actions[state])
-            forward = (back + 2) % len(state_actions[state])
-            # Viable actions is a dictionary from 'left', 'right', and 'backward' and are mapped to their back, left, and right indices in the adjacency dict
-            viable_actions = {'left': left, 'right': right, 'forward': forward, 'back': back}
-        elif len(state_actions[state]) == 5 :
-            left = (back + 1) % len(state_actions[state])
-            right = (back - 1) % len(state_actions[state])
-            forward_left = (back + 2) % len(state_actions[state])
-            forward_right = (back + 3) % len(state_actions[state])
-            # Viable actions is a dictionary from 'left', 'right', and 'backward' and are mapped to their back, left, and right indices in the adjacency dict
-            viable_actions = {'left': left, 'right': right, 'forward_left': forward_left, 'forward_right': forward_right, 'back': back}
-        else :
-            left = (back + 1) % len(state_actions[state])
-            right = (back - 1) % len(state_actions[state])
-            forward_left = (back + 2) % len(state_actions[state])
-            forward_right = (back - 2) % len(state_actions[state])
-            forward = (back + 3) % len(state_actions[state])
-            # Viable actions is a dictionary from 'left', 'right', and 'backward' and are mapped to their back, left, and right indices in the adjacency dict
-            viable_actions = {'left': left, 'right': right, 'forward_left': forward_left, 'forward_right': forward_right, 'forward': forward, 'back': back}
-
-
-
-        # geting the list of actions and their corresponding Q-values
-        actions = list(viable_actions.keys())
-        q_values = [q_table[(state, a)] for a in actions]
-
-        # normalize the Q-values to form a probability distribution
-        soft_q_values = np.exp(q_values)  # use softmax to ensure the Q-values are positive
-        prob_distribution = soft_q_values / np.sum(soft_q_values)
-
-        # select an action based on the Q-values
-        action = np.random.choice(actions, p=prob_distribution)
-        index = viable_actions[action]
-
-
-    # greedy action selection :
-    else:
-        # Action going back
-        back = [a for a in range(len(state_actions[state])) if state_actions[state][a] == previous_state][0]
-
-        # Action left & right
-        if len(state_actions[state]) == 2:
-            forward = (back + 1) % len(state_actions[state])
-            viable_actions = {'forward': forward, 'back': back}
-        elif len(state_actions[state]) == 3 :
-            left = (back + 1) % len(state_actions[state])
-            right = (back - 1) % len(state_actions[state])
-            # Viable actions is a dictionary from 'left', 'right', and 'backward' and are mapped to their back, left, and right indices in the adjacency dict
-            viable_actions = {'left': left, 'right': right, 'back': back}
-        elif len(state_actions[state]) == 4 :
-            left = (back + 1) % len(state_actions[state])
-            right = (back - 1) % len(state_actions[state])
-            forward = (back + 2) % len(state_actions[state])
-            # Viable actions is a dictionary from 'left', 'right', and 'backward' and are mapped to their back, left, and right indices in the adjacency dict
-            viable_actions = {'left': left, 'right': right, 'forward': forward, 'back': back}
-        elif len(state_actions[state]) == 5 :
-            left = (back + 1) % len(state_actions[state])
-            right = (back - 1) % len(state_actions[state])
-            forward_left = (back + 2) % len(state_actions[state])
-            forward_right = (back + 3) % len(state_actions[state])
-            # Viable actions is a dictionary from 'left', 'right', and 'backward' and are mapped to their back, left, and right indices in the adjacency dict
-            viable_actions = {'left': left, 'right': right, 'forward_left': forward_left, 'forward_right': forward_right, 'back': back}
-        else :
-            left = (back + 1) % len(state_actions[state])
-            right = (back - 1) % len(state_actions[state])
-            forward_left = (back + 2) % len(state_actions[state])
-            forward_right = (back - 2) % len(state_actions[state])
-            forward = (back + 3) % len(state_actions[state])
-            # Viable actions is a dictionary from 'left', 'right', and 'backward' and are mapped to their back, left, and right indices in the adjacency dict
-            viable_actions = {'left': left, 'right': right, 'forward_left': forward_left, 'forward_right': forward_right, 'forward': forward, 'back': back}
-
-
-        # Select the action with the highest Q-value
-        action = max(viable_actions, key=lambda a: q_table[(state, a)])
-        index = viable_actions[action]
-
-    return action, index
-
-
-def train(graph, goal_coord, method, q_table, state_actions_dist, episodes=5000, alpha=0.1, gamma=0.9, initial_epsilon=0.1, min_epsilon=0.01, max_distance=500, initial_q=1):
-    """
-    Train the agent using SARSA with e-greedy action selection.
-    Update Agent-Agnostic EXP3-IX algorithm during training
-    """
-    Q = q_table.copy()
-    Qs = []
-
-    cce = {}
-
-    # Initialize new states and actions in the Q-table
-    for state, action_dists in state_actions_dist.items():
-        actions = [ad[0] for ad in action_dists]  # Extract actions (neighbor labels) from tuples
-
-        # Determine the action strings based on the number of actions
-        if len(actions) == 2:
-            acts = actions_edge
-        elif len(actions) == 3:
-            acts = actions_main
-        elif len(actions) == 4:
-            acts = actions_four
-        elif len(actions) == 5:
-            acts = actions_five
-        elif len(actions) == 6:
-            acts = actions_six
-
-        # Check if all action strings for this state are in the Q-table
-        for i, action in enumerate(actions):
-            if (state, acts[i]) not in Q:
-                Q[(state, acts[i])] = initial_q
-
-    epsilon = initial_epsilon
-    goal = get_index(graph, goal_coord)
-
-    for episode in range(episodes):
-        state = np.random.choice(list(graph.nodes))
-        previous_state, _ = random.choice(state_actions_dist[state])
-        distance = 0
-
-        # Initial Action selection
-        action, action_idx = action_selection(graph, state_actions_dist, Q, state, previous_state, epsilon=epsilon)
-
-        while state != goal and distance < max_distance:
-
-            # State update based on action
-            new_state, dist = state_actions_dist[state][action_idx]
-
-            # Action selection for next action (for SARSA update rule)
-            next_action, next_action_idx = action_selection(graph, state_actions_dist, Q, new_state, state, epsilon=epsilon)
-
-            distance += dist
-
-            reward = 0 if new_state != goal else (1 - distance)
-
-            # SARSA Update Rule
-            Q[(state, action)] = Q[(state, action)] + alpha * (reward + gamma * Q.get((new_state, next_action), initial_q) - Q[(state, action)])
-            # CCE Update 
-            cce = update_eq(cce, state, action, reward, T = episodes)
-
-            action = next_action
-            action_idx = next_action_idx
-            previous_state = state
-            state = new_state
-
-        # Decaying epsilon with episodes
-        epsilon = max(min_epsilon, epsilon / np.sqrt((episode + 1)))
-
-        if (episode + 1) % (episodes / 5) == 0:
-            Qs.append(Q)
-
-    return Qs, cce
-
-def initialize_or_update_cce(cce, state, action):
-    # s = tuple(state)
-    s = state
-    if s not in cce:
-        cce[s] = {}
-
-    if action not in cce[s]:
-        # Check if there are any actions already for this state
-        if cce[s]:  # If there are existing actions
-            # Initialize as the average approx for all actions for this state, and visit count as 0
-            avg_approx = np.mean([cce[s][a][0] for a in cce[s]])
-        else:
-            # If no actions exist, initialize to 1.0
-            avg_approx = 1.0
-        cce[s][action] = [avg_approx, 0]
-
-    return cce
-
-def update_eq(cce, state, action, loss, T=100):
-    # state = tuple(state)
-    cce = initialize_or_update_cce(cce, state, action)
-
-    eq_approx_dict = {act: val[0] for act, val in cce[state].items()}
-    visit_count_dict = {act: val[1] for act, val in cce[state].items()}
-    total_eq_approx = sum(eq_approx_dict.values())
-    policy = {act: eq_approx_dict[act] / total_eq_approx for act in eq_approx_dict}
-
-    # Hyperparameter updates:
-    gamma = np.sqrt(2 * np.log(len(policy)) / (len(policy) * T))
-    eta = 2 * gamma
-
-    estimated_loss = loss / (policy[action] + gamma)
-
-    log_probs = {act: np.log(policy[act] + 1e-50) for act in policy}
-    log_probs[action] -= eta * estimated_loss
-
-    max_log_prob = max(log_probs.values())
-    for act in eq_approx_dict:
-        eq_approx_dict[act] = np.exp(log_probs[act] - max_log_prob)
-        cce[state][act][0] = eq_approx_dict[act]
-
-    visit_count_dict[action] += 1
-    cce[state][action][1] = visit_count_dict[action]
-
-    return cce
-
-
 
 ############################### TESTING #################################
 
@@ -664,12 +423,12 @@ def test(cce, q_table, state_actions_dist, graph, start_coordinates, goal_coordi
 
         # Check if the goal is reached
         if new_state == goal_index:
-            cce_precentage = num_cce_actions / (num_cce_actions + num_sarsa_actions)
             break
 
         previous_state = state
         state = new_state
 
+    cce_precentage = num_cce_actions / (num_cce_actions + num_sarsa_actions)
     return path_coords, path_indices, total_distance, cce_precentage
 
 
@@ -684,9 +443,12 @@ temp = 0.01
 
 ALL_DISTS = []
 ALL_PATHS = []
-ALL_Q_TABLES = []
-ALL_CCE = []
 ALL_CCE_PRECENTAGES = []
+ALL_ZETAS = []
+
+Q_tables = load.Q_tables
+CCEs = load.cces
+graph = load.graph
 
 ############################## START LOOP ##############################
 
@@ -702,33 +464,17 @@ voronoi_diagrams_sites = sites
 
 ### Trials ###
 for trial in range(TRIALS) :
-    # if trial % 10 == 0 :
-    print(f'Trial {trial} of {TRIALS}')
+    ALL_PATHS.append([])
+    ALL_DISTS.append([])
+    ALL_CCE_PRECENTAGES.append([])
+    ALL_ZETAS.append([])
 
-    star = 0
-    hex = 0
-
-
-    label = label_voronoi_sites(voronoi_diagrams_sites, voronoi_diagram, voronoi_diagrams_sites)
-
-
-    graph = voronoi_to_graph(voronoi_diagram, label)
+    cce =CCEs[trial]
+    q_table = Q_tables[trial]
 
     North_index, North_coord, South_index, South_coord, East_index, East_coord, West_index, West_coord = find_extreme_vertices(voronoi_diagram)
-
     state_actions_dist = neighbors(graph)
 
-    # TRAINING ON VORONOI DIAGRAM
-
-    init_q_table = create_q_table(state_actions_dist, init_value=1.0)
-
-    q_table, cce = train(graph = graph,
-                                goal_coord = East_coord,
-                                method = 'egreedy',
-                                q_table = init_q_table,
-                                state_actions_dist = state_actions_dist,
-                                initial_epsilon = 0.25,
-                                episodes = 500)
 
     # TESTING PATH AND DISTANCE MEASURE AFTER TRAINING
     for zeta in zetas:
@@ -737,17 +483,15 @@ for trial in range(TRIALS) :
                                                         West_coord, East_coord, 
                                                         zeta, max_steps = 100000, temperature = 0.1)
 
-        ALL_PATHS.append(path_coords)
-        ALL_DISTS.append(path_dist)
-        ALL_Q_TABLES.append(q_table)
-        ALL_CCE.append(cce)
-        ALL_CCE_PRECENTAGES.append(propCCE)
+        ALL_PATHS[trial].append(path_coords)
+        ALL_DISTS[trial].append(path_dist)
+        ALL_ZETAS[trial].append(zeta)
+        ALL_CCE_PRECENTAGES[trial].append(propCCE)
 
 ###################### SAVE OUTPUTS ######################
 
 import pickle
 
-folder = '/scratch/egraham/CASTLE-GT/RL/graphical-implementation/runs'
 os.makedirs(folder, exist_ok=True)
 
 
@@ -757,14 +501,12 @@ with open(os.path.join(folder, 'SARSA_dists.pkl'), 'wb') as f:
 with open(os.path.join(folder, 'SARSA_paths.pkl'), 'wb') as f:
     pickle.dump(ALL_PATHS, f)
 
-with open(os.path.join(folder, 'SARSA_q_tables.pkl'), 'wb') as f:
-    pickle.dump(ALL_Q_TABLES, f)
-
-with open(os.path.join(folder, 'SARSA_cce.pkl'), 'wb') as f:
-    pickle.dump(ALL_CCE, f)
 
 with open(os.path.join(folder, 'SARSA_cce_percentages.pkl'), 'wb') as f:
     pickle.dump(ALL_CCE_PRECENTAGES, f)
 
 with open(os.path.join(folder, 'zetavalues.pkl'), 'wb') as f:
     pickle.dump(zetas, f)
+
+with open(os.path.join(folder, 'all_zetas.pkl'), 'wb') as f:
+    pickle.dump(ALL_ZETAS, f)
